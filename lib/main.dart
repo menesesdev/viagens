ffcde95af63c6010d39fe99ecbaf0735c44a10d0
Mateus Meneses Rocha <contato.menesesdev@gmail.com>
2023-05-03 16:22:48 -0300

    Estilizando o Header

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Color.fromARGB(255, 13, 105, 255),
       body: ListView(
         children: <Widget>[
           Stack(
@@ -37,7 +38,74 @@ class Home extends StatelessWidget {
                 child: Image.asset(
                   'assets/images/capa.jpg',
                   fit: BoxFit.cover,
+                  color:
+                      const Color.fromARGB(255, 13, 105, 255).withOpacity(0.6),
+                  colorBlendMode: BlendMode.softLight,
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                      color: Colors.white,
+                    ),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: FractionalOffset.topCenter,
+                          end: FractionalOffset.bottomCenter,
+                          colors: [
+                            const Color(0xff0d69ff).withOpacity(0.0),
+                            const Color(0xff0069ff).withOpacity(0.8),
+                          ],
+                        ),
+                      ),
+                    ),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: FractionalOffset.topLeft,
+                            end: FractionalOffset.bottomRight,
+                            colors: [
+                              const Color(0xff692eff).withOpacity(0.8),
+                              const Color(0xff642cf4).withOpacity(0.8),
+                              const Color(0xff602ae9).withOpacity(0.8),
+                              const Color(0xff5224c8).withOpacity(0.8),
+                              const Color(0xff5e29e5).withOpacity(0.8),
+                            ],
+                            stops: const [
+                              0.0,
+                              0.25,
+                              0.5,
+                              0.75,
+                              1.0
+                            ]),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.search),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              const Center(
+                child: Padding(
+                    padding: EdgeInsets.only(top: 180),
+                    child: Text(
+                      "CONHEÇA O MUNDO",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    )),
               )
             ],
           )
