240ed85c2ef599dc5debe72cb70870eb3ba3a57a
menesesdev <menesescriativa@gmail.com>
2023-05-03 10:14:03 -0300

    Init. Cód. - Add. Font.

---
+++ android/app/src/main/kotlin/com/example/viagens/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.viagens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Stack(
+            children: [
+              Image.asset(''),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
